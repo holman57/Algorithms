3e508a50e4e7569bcb8323b64ed45614ce7af7c9
Luke Holman <holman57@gmail.com>
2025-02-15 10:16:43 -0600

    Rename test function and fix newline issue
    Renamed the test function in `SumOfThreeValues.py` to improve readability and clarity. Additionally, fixed the missing newline at the end of `SortColors.py` to align with standard formatting practices.

--- TwoPointers/SumOfThreeValues.py
+++ TwoPointers/SumOfThreeValues.py
@@ -29,12 +29,12 @@ def three_sum(N):
     return res
 
 
-def test():
+def test_three_sum():
     assert three_sum([-1, 0, 1, 2, -1, -4]) == [[-1, -1, 2], [-1, 0, 1]]
     assert three_sum([]) == []
     assert three_sum([0]) == []
     assert three_sum([0, 0, 0]) == [[0, 0, 0]]
 
 
-test()
+test_three_sum()
 print("All test cases passed!")
